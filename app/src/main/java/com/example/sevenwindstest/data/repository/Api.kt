a924ef0b6bf66e3f69b9f28c6d732850070668b3
Kirill <ananevka@mail.ru>
2025-07-17 16:54:54 +0300

    api interface

---
+++ app/src/main/java/com/example/sevenwindstest/data/repository/Api.kt
@@ -0,0 +1,29 @@
+package com.example.sevenwindstest.data.repository
+
+import com.example.sevenwindstest.data.dto.LoginRequest
+import com.example.sevenwindstest.data.dto.LoginResponse
+import com.example.sevenwindstest.data.dto.RegistrationRequest
+import com.example.sevenwindstest.data.dto.RegistrationResponse
+import com.example.sevenwindstest.data.dto.Location
+import retrofit2.Response
+
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface Api {
+
+    @POST("login")
+    suspend fun login(@Body request: LoginRequest): Response<LoginResponse>
+
+    @POST("registration")
+    suspend fun registration(@Body request: RegistrationRequest): Response<RegistrationResponse>
+
+    @GET("locations")
+    suspend fun getLocations(): Response<List<Location>>
+
+    @GET("locations/{id}")
+    suspend fun getLocationById(@Path("id") id: Long): Response<Location>
+}
+
