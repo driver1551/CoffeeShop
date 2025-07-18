73df72ca90f8e9fb398ddd22855262d2305b8cb2
Kirill <ananevka@mail.ru>
2025-07-18 10:41:09 +0300

    + navigate to login if registered

--- app/src/main/java/com/example/sevenwindstest/ui/registration/RegistrationViewModel.kt
+++ app/src/main/java/com/example/sevenwindstest/ui/registration/RegistrationViewModel.kt
@@ -1,35 +1,53 @@
 package com.example.sevenwindstest.ui.registration
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.sevenwindstest.domain.usecase.RegistrationUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class RegistrationViewModel @Inject constructor(
-
+    private val registrationUseCase: RegistrationUseCase
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(RegistrationUiState())
     val uiState: StateFlow<RegistrationUiState> = _uiState.asStateFlow()
 
+    private val _navigateToLogin = MutableSharedFlow<Unit>()
+    val navigateToLogin: SharedFlow<Unit> = _navigateToLogin.asSharedFlow()
+
+
     fun onEmailChange(newEmail: String) {
-        _uiState.update { it.copy(email = newEmail) }
+        _uiState.setEmail(newEmail)
     }
 
     fun onPasswordChange(newPassword: String) {
-        _uiState.update { it.copy(password = newPassword) }
+        _uiState.setPassword(newPassword)
     }
 
     fun onRepeatPasswordChange(newRepeatPassword: String) {
-        _uiState.update { it.copy(repeatPassword = newRepeatPassword) }
+        _uiState.setRepeatPassword(newRepeatPassword)
     }
 
     fun onRegisterClick() {
-        // Пока заглушка, позже вызовем Repository
-        println("Регистрация: ${_uiState.value.email}, ${_uiState.value.password}")
+        viewModelScope.launch {
+            _uiState.setLoading(true)
+            val result = registrationUseCase.invoke(_uiState.value.email, _uiState.value.password)
+            if (result.isSuccess) {
+                _navigateToLogin.emit(Unit)
+            } else {
+                _uiState.setErrorMessage(result.toString())
+                _navigateToLogin.emit(Unit) // заглушка
+            }
+            _uiState.setLoading(false)
+        }
     }
 }
