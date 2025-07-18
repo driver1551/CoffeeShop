e3c42d07bf4871042658d60070cda2d7efccdf06
Kirill <ananevka@mail.ru>
2025-07-18 10:41:18 +0300

    + extensions

--- app/src/main/java/com/example/sevenwindstest/ui/registration/RegistrationUiState.kt
+++ app/src/main/java/com/example/sevenwindstest/ui/registration/RegistrationUiState.kt
@@ -1,9 +1,32 @@
 package com.example.sevenwindstest.ui.registration
 
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+
 data class RegistrationUiState(
+    val isLoading: Boolean = false,
     val email: String = "",
     val password: String = "",
     val repeatPassword: String = "",
-    val isLoading: Boolean = true,
     val errorMessage: String? = null
 )
+
+fun MutableStateFlow<RegistrationUiState>.setLoading(isLoading: Boolean) {
+    update { it.copy(isLoading = isLoading) }
+}
+
+fun MutableStateFlow<RegistrationUiState>.setEmail(email: String) {
+    update { it.copy(email = email) }
+}
+
+fun MutableStateFlow<RegistrationUiState>.setPassword(password: String) {
+    update { it.copy(password = password) }
+}
+
+fun MutableStateFlow<RegistrationUiState>.setRepeatPassword(repeatPassword: String) {
+    update { it.copy(repeatPassword = repeatPassword) }
+}
+
+fun MutableStateFlow<RegistrationUiState>.setErrorMessage(errorMessage: String) {
+    update { it.copy(errorMessage = errorMessage) }
+}
