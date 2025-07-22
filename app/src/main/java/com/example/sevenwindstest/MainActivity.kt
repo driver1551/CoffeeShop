c5d24da0611762d1cb8be1a0b69515838e1c5c42
Kirill <ananevka@mail.ru>
2025-07-22 11:31:45 +0300

    MapKitFactory init

--- app/src/main/java/com/example/sevenwindstest/MainActivity.kt
+++ app/src/main/java/com/example/sevenwindstest/MainActivity.kt
@@ -3,16 +3,25 @@ package com.example.sevenwindstest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
 import com.example.sevenwindstest.navigation.AppNavGraph
+import com.example.sevenwindstest.ui.common.LocationPermissionRequest
 import com.example.sevenwindstest.ui.theme.SevenWindsTestTheme
+import com.yandex.mapkit.MapKitFactory
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Yandex mapkit setup
+        MapKitFactory.setApiKey("f88b28ed-6620-4bc2-9c30-b397664b8bbe")
+        MapKitFactory.initialize(this.applicationContext)
+        MapKitFactory.getInstance().onStart()
+
         setContent {
+            LocationPermissionRequest()
+
             SevenWindsTestTheme {
                 AppNavGraph()
             }
